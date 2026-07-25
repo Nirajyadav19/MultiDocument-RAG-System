6e3d34b9abbca76707d1287bbfed766610c5dea4
niraj yadav <niraj19082002@gmail.com>
2026-07-25 16:51:29 +0530

    first commit

--- src/create_vector_db.py
+++ src/create_vector_db.py
@@ -1,9 +1,9 @@
 from langchain_chroma import Chroma
 from embedding import embedding_model, text_to_chunk
 
-pdf_path1="C:/Users/Downloads/rag (2)/rag/data/SQL-Manual.pdf"
+pdf_path1="D:/Niraj/rag/SQL-Manual.pdf"
 
-pdf_path2="C:/Users/Downloads/rag (2)/rag/data/Niraj_Yadav_Resume.pdf"
+pdf_path2="D:/Niraj/rag/machine-learning-book.pdf"
 
 chunks1 = text_to_chunk(pdf_path1)
 
@@ -13,14 +13,14 @@ Chroma.from_documents(
     documents=chunks1,
     embedding=embedding_model,
     collection_name="sql",
-    persist_directory="D:/Niraj/Agentic ai/rag/vector_db"
+    persist_directory="D:/Niraj/rag/vector_db"
 )
 
 Chroma.from_documents(
     documents=chunks2,
-    collection_name="niraj",
+    collection_name="machine_learning",
     embedding=embedding_model,
-    persist_directory="D:/Niraj/Agentic ai/rag/vector_db"
+    persist_directory="D:/Niraj/rag/vector_db"
 )
 
 print("Vector DB created successfully!")
--- src/vector_store.py
+++ src/vector_store.py
@@ -4,13 +4,13 @@ from embedding import embedding_model
 embeddings = embedding_model
 
 vector_store1 = Chroma(
-    persist_directory="D:/Niraj/Agentic ai/rag/vector_db",
+    persist_directory="D:/Niraj/rag/vector_db",
     collection_name="sql",
     embedding_function=embeddings
 )
 
 vector_store2 = Chroma(
-    persist_directory="D:/Niraj/Agentic ai/rag/vector_db",
-    collection_name="niraj",
+    persist_directory="D:/Niraj/rag/vector_db",
+    collection_name="machine_learning",
     embedding_function=embeddings
 )
